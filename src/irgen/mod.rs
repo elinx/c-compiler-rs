ece8aee6f15071f8da87abc7a5b1a6c0de86df10
Elinx Hsi <xizzuli@163.com>
2021-05-07 23:02:41 +0800

    binary operator expression

--- src/irgen/mod.rs
+++ src/irgen/mod.rs
@@ -1,12 +1,15 @@
 use core::fmt;
-use std::collections::{BTreeMap, HashMap};
 use std::mem;
 use std::ops::Deref;
+use std::{
+    collections::{BTreeMap, HashMap},
+    convert::TryFrom,
+};
 
 use failure::Fail;
 use lang_c::ast::*;
 
-use crate::ir::BlockExit;
+use crate::ir::{BlockExit, BlockId, Dtype, Instruction, Named, Operand, RegisterId};
 use crate::*;
 use crate::{ir::DtypeError, ir::FunctionSignature, write_base::WriteString};
 
@@ -21,8 +24,56 @@ pub struct IrgenError {
     pub message: IrgenErrorMessage,
 }
 
+#[derive(Debug)]
+struct BBContext {
+    bid: BlockId,
+    instructions: Vec<Named<Instruction>>,
+}
+
+impl BBContext {
+    pub fn new(bid: BlockId) -> Self {
+        Self {
+            bid,
+            instructions: Vec::new(),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct FunctionContext {
+    // return_type
+    allocations: Vec<Named<Dtype>>,
+    blocks: BTreeMap<ir::BlockId, ir::Block>,
+    bid_counter: usize,
+    tempid_counter: usize,
+    // typedefs
+    symbol_table: Vec<String>,
+    curr_block: BBContext,
+}
+
+impl FunctionContext {
+    fn new() -> Self {
+        Self {
+            allocations: Vec::new(),
+            blocks: BTreeMap::new(),
+            bid_counter: 1,
+            tempid_counter: 0,
+            symbol_table: Vec::new(),
+            curr_block: BBContext::new(BlockId(1)),
+        }
+    }
+
+    #[allow(dead_code)]
+    fn alloc_bid(&mut self) -> BlockId {
+        let bid = BlockId(self.bid_counter);
+        self.bid_counter += 1;
+        bid
+    }
+}
 #[derive(Debug, PartialEq, Fail)]
 pub enum IrgenErrorMessage {
+    #[fail(display = "{}", message)]
+    Misc { message: String },
     #[fail(display = "{}", dtype_error)]
     InvalidDtype { dtype_error: DtypeError },
 }
@@ -97,37 +148,273 @@ impl Irgen {
             )
         })?;
         println!("dtype: {}", dtype.name().unwrap_or(&"unamed".to_owned()));
-        // create a block which only contains a `return 0;` statement
-        let value = ir::Constant::Int {
-            value: 1,
-            width: 4,
-            is_signed: false,
-        };
-        let exit = BlockExit::Return {
-            value: ir::Operand::Constant(value),
-        };
-        let block = ir::Block {
-            phinodes: Vec::new(),
-            instructions: Vec::new(),
-            exit,
-        };
-        let mut blocks = BTreeMap::new();
-        blocks.insert(ir::BlockId(0), block);
+
         let signature = FunctionSignature::new(dtype.deref().clone());
+        let mut func_ctx = FunctionContext::new();
+        self.translate_stmt(&func.statement.node, &mut func_ctx)?;
+
         let definition = Some(ir::FunctionDefinition {
-            allocations: Vec::new(),
-            blocks,
+            allocations: func_ctx.allocations,
+            blocks: func_ctx.blocks,
             bid_init: ir::BlockId(0),
         });
-        let function = ir::Declaration::Function {
-            signature,
-            definition,
-        };
-        self.decls.insert(func_name, function);
+        self.decls.insert(
+            func_name,
+            ir::Declaration::Function {
+                signature,
+                definition,
+            },
+        );
         Ok(())
     }
 
     fn name_of_declarator(&self, declarator: &Declarator) -> String {
         declarator.kind.write_string()
+    }
+
+    fn translate_stmt(
+        &self,
+        statement: &Statement,
+        func_ctx: &mut FunctionContext,
+    ) -> Result<(), IrgenError> {
+        match statement {
+            Statement::Labeled(_) => {}
+            Statement::Compound(block_items) => {
+                for block_item in block_items {
+                    self.translate_block_item(&block_item.node, func_ctx)?;
+                }
+            }
+            Statement::Expression(_) => {}
+            Statement::If(_) => {}
+            Statement::Switch(_) => {}
+            Statement::While(_) => {}
+            Statement::DoWhile(_) => {}
+            Statement::For(_) => {}
+            Statement::Goto(_) => {}
+            Statement::Continue => {}
+            Statement::Break => {}
+            Statement::Return(return_stmt) => {
+                if let Some(expr) = return_stmt {
+                    let value = self.translate_expression(&expr.deref().node, func_ctx)?;
+                    let exit = BlockExit::Return { value };
+                    let block = ir::Block {
+                        phinodes: Vec::new(),
+                        instructions: std::mem::replace(
+                            &mut func_ctx.curr_block.instructions,
+                            Vec::new(),
+                        ),
+                        exit,
+                    };
+                    func_ctx.blocks.insert(func_ctx.curr_block.bid, block);
+                    println!("{:#?}", func_ctx);
+                }
+            }
+            Statement::Asm(_) => {}
+        }
+        Ok(())
+    }
+
+    fn translate_block_item(
+        &self,
+        block_item: &BlockItem,
+        func_ctx: &mut FunctionContext,
+    ) -> Result<(), IrgenError> {
+        match block_item {
+            BlockItem::Declaration(ref decl) => {
+                self.translate_declaration(&decl.node, func_ctx)?;
+            }
+            BlockItem::StaticAssert(_) => unimplemented!("static assert"),
+            BlockItem::Statement(stmt) => {
+                self.translate_stmt(&stmt.node, func_ctx)?;
+            }
+        }
+        Ok(())
+    }
+
+    fn translate_declaration(
+        &self,
+        decl: &Declaration,
+        func_ctx: &mut FunctionContext,
+    ) -> Result<(), IrgenError> {
+        let (dtype, is_typedef) = ir::Dtype::try_from_ast_declaration_specifiers(&decl.specifiers)
+            .map_err(|e| {
+                IrgenError::new(
+                    decl.write_string(),
+                    IrgenErrorMessage::InvalidDtype { dtype_error: e },
+                )
+            })?;
+        println!("Dtype: {}, is_typedef: {}", dtype, is_typedef);
+        for declarator in &decl.declarators {
+            let name = declarator.node.declarator.write_string();
+
+            let allocation = Named::new(Some(name), dtype.clone());
+            func_ctx.allocations.push(allocation);
+            let aid = func_ctx.allocations.len() - 1;
+            let rid = RegisterId::Local { aid };
+            let ptr = Operand::register(rid, Dtype::pointer(dtype.clone()));
+
+            // translate initializer
+            if let Some(ref initializer) = declarator.node.initializer {
+                match initializer.node {
+                    Initializer::Expression(ref expr) => {
+                        let operand = self.translate_expression(&expr.deref().node, func_ctx)?;
+                        let instr = Instruction::Store {
+                            ptr,
+                            value: operand,
+                        };
+                        func_ctx
+                            .curr_block
+                            .instructions
+                            .push(Named::new(Some("abc".to_owned()), instr));
+                    }
+                    Initializer::List(_) => unimplemented!("list initializer"),
+                }
+            }
+        }
+        Ok(())
+    }
+
+    fn translate_expression(
+        &self,
+        expression: &Expression,
+        func_ctx: &mut FunctionContext,
+    ) -> Result<Operand, IrgenError> {
+        match expression {
+            Expression::Identifier(id) => self.translate_identifier(&id.deref().node, func_ctx),
+            Expression::Constant(const_value) => ir::Constant::try_from(&const_value.deref().node)
+                .map(|val| Operand::Constant(val))
+                .map_err(|_| {
+                    IrgenError::new(
+                        const_value.write_string(),
+                        IrgenErrorMessage::Misc {
+                            message: "invlaid constant".to_owned(),
+                        },
+                    )
+                }),
+            Expression::StringLiteral(_) => todo!("string literal"),
+            Expression::GenericSelection(_) => todo!("generic selection"),
+            Expression::Member(_) => todo!("member"),
+            Expression::Call(_) => todo!("call"),
+            Expression::CompoundLiteral(_) => todo!("compound literal"),
+            Expression::SizeOf(_) => todo!("sizeof"),
+            Expression::AlignOf(_) => todo!("alignof"),
+            Expression::UnaryOperator(unary) => {
+                self.translate_unary_operator(&unary.deref().node, func_ctx)
+            }
+            Expression::Cast(_) => todo!("cast"),
+            Expression::BinaryOperator(bop) => {
+                self.translate_binary_operator(&bop.deref().node, func_ctx)
+            }
+            Expression::Conditional(_) => todo!("conditional"),
+            Expression::Comma(_) => todo!("comma"),
+            Expression::OffsetOf(_) => todo!("offsetof"),
+            Expression::VaArg(_) => todo!("vaarg"),
+            Expression::Statement(_) => todo!("stmt"),
+        }
+    }
+
+    fn translate_unary_operator(
+        &self,
+        unary: &UnaryOperatorExpression,
+        func_ctx: &mut FunctionContext,
+    ) -> Result<Operand, IrgenError> {
+        // load %l0:u8* reg:tmp:1
+        // reg:tmp:2 = sub reg:tmp1 const:1
+        // store reg:tmp:2 %l0:u8*
+        let op = unary.operator.node.clone();
+        let operand = self.translate_expression(&unary.operand.node, func_ctx)?;
+        println!("operand: {:?}", operand);
+        let (rid, dtype) = match operand {
+            Operand::Constant(_) => {
+                return Err(IrgenError::new(
+                    unary.write_string(),
+                    IrgenErrorMessage::Misc {
+                        message: "unary operator on const value is illegal".to_owned(),
+                    },
+                ))
+            }
+            Operand::Register {
+                ref rid, ref dtype, ..
+            } => (rid, dtype.clone()),
+        };
+        match op {
+            // UnaryOperator::PostIncrement => {}
+            // UnaryOperator::PostDecrement => {}
+            // UnaryOperator::PreIncrement => {}
+            UnaryOperator::PreDecrement => {
+                let one = Operand::constant(ir::Constant::Int {
+                    value: 1,
+                    width: 8,
+                    is_signed: false,
+                });
+                let sub_instr = Instruction::BinOp {
+                    op: BinaryOperator::Minus,
+                    lhs: operand,
+                    rhs: one,
+                    dtype,
+                };
+                func_ctx
+                    .curr_block
+                    .instructions
+                    .push(Named::new(None, sub_instr));
+
+                let iid = func_ctx.curr_block.instructions.len() - 1;
+                let res_operand = RegisterId::temp(func_ctx.curr_block.bid, iid);
+                let store_instr = Instruction::Store {
+
+                }
+            }
+            // UnaryOperator::Address => {}
+            // UnaryOperator::Indirection => {}
+            // UnaryOperator::Plus => {}
+            // UnaryOperator::Minus => {}
+            // UnaryOperator::Complement => {}
+            // UnaryOperator::Negate => {}
+            // UnaryOperator::SizeOf => {}
+            _ => todo!("more unary operators"),
+        }
+
+        let store_instr = Instruction::UnaryOp {
+            op,
+            operand: operand.clone(),
+            dtype,
+        };
+        func_ctx
+            .curr_block
+            .instructions
+            .push(Named::new(None, instr));
+        Ok(operand)
+    }
+
+    fn translate_binary_operator(
+        &self,
+        binary: &BinaryOperatorExpression,
+        func_ctx: &mut FunctionContext,
+    ) -> Result<Operand, IrgenError> {
+        let _op = binary.operator.node.clone();
+        let _rhs = self.translate_expression(&binary.rhs.node, func_ctx)?;
+        let lhs = self.translate_expression(&binary.lhs.node, func_ctx)?;
+        // let instr = Instruction::BinOp { op, lhs, rhs };
+        Ok(lhs)
+    }
+
+    fn translate_identifier(
+        &self,
+        _identifier: &Identifier,
+        func_ctx: &mut FunctionContext,
+    ) -> Result<Operand, IrgenError> {
+        // TODO: lookup identifier name form symbol table.
+        let aid = func_ctx.allocations.len() - 1;
+        let dtype = &func_ctx.allocations[aid];
+        let rid = RegisterId::Local { aid };
+        let ptr = Operand::register(rid, Dtype::pointer(dtype.deref().clone()));
+
+        let instr = Instruction::Load { ptr: ptr.clone() };
+        func_ctx
+            .curr_block
+            .instructions
+            .push(Named::new(None, instr));
+
+        Ok(ptr)
     }
 }
