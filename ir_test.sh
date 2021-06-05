41c0170b4a1a038ce69befa5f5af8656b8440bfd
Elinx Hsi <xizzuli@163.com>
2021-06-05 10:19:25 +0800

    add all testcases, early exit for failed testcase

--- ir_test.sh
+++ ir_test.sh
@@ -5,13 +5,54 @@ test_cases=(
     array.c
     array2.c
     array3.c
+    array4.c
+    array5.c
+    bar.c
+    bitwise.c
+    cmp.c
+    comma.c
+    complete_cond_gen.py
+    complete_cond.c
+    cond_and_loop.c
+    cond.c
+    fib2.c
+    fib3.c
+    fib4.c
+    fib5.c
+    fibonacci.c
+    float.c
+    float2_gen.py
+    float2.c
+    foo.c
+    foo2.c
+    foo3.c
+    foo4.c
+    for_continue_break.c
     gcd.c
+    integer_literal.c
+    integer_literal2.c
     logical_op.c
+    minus_constant.c
+    negate.c
     pointer.c
     return_void.c
+    shift.c
+    simple_cond.c
     simple_for.c
     simple_if.c
-    simple_cond.c
+    simple.c
+    sizeof.c
+    struct.c
+    struct2.c
+    struct3.c
+    switch.c
+    temp.c
+    temp2.c
+    test.c
+    typecast.c
+    typedef.c
+    unary.c
+    while_continue_break.c
 )
 
 for test_case in "${test_cases[@]}"; do
@@ -19,5 +60,6 @@ for test_case in "${test_cases[@]}"; do
     if [ $? -ne 0 ]; then
         cargo run -- -i "examples/c/${test_case}" | tee temp.ir
         diff temp.ir "examples/ir0/${test_case%c}ir"
+        exit 1
     fi
 done
