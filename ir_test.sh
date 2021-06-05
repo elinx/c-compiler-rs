d8b53ad2547fe0f4e05af2c2efe629fb081b8d53
Elinx Hsi <xizzuli@163.com>
2021-06-05 10:00:54 +0800

    refactor regression script

--- ir_test.sh
+++ ir_test.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
-cargo run --release --bin fuzz examples/c/alignof.c -i
-cargo run --release --bin fuzz examples/c/array.c -i
-cargo run --release --bin fuzz examples/c/array2.c -i
-cargo run --release --bin fuzz examples/c/array3.c -i
-cargo run --release --bin fuzz examples/c/gcd.c -i
-cargo run --release --bin fuzz examples/c/pointer.c -i
-cargo run --release --bin fuzz examples/c/simple_for.c -i
+
+test_cases=(
+    alignof.c
+    array.c
+    array2.c
+    array3.c
+    gcd.c
+    pointer.c
+    return_void.c
+    simple_for.c
+    simple_if.c
+    simple_cond.c
+)
+
+for test_case in "${test_cases[@]}"; do
+    cargo run --release --bin fuzz "examples/c/${test_case}" -i
+    if [ $? -ne 0 ]; then
+        cargo run -- -i "examples/c/${test_case}"
+    fi
+done
