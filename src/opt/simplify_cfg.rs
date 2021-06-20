c2539f70bcf143445283322b0982ea58abe3ddff
Elinx Hsi <xizzuli@163.com>
2021-06-20 09:09:03 +0800

    simple exit block constant optimization

--- src/opt/simplify_cfg.rs
+++ src/opt/simplify_cfg.rs
@@ -1,3 +1,5 @@
+use std::ops::Deref;
+
 use crate::ir::*;
 use crate::opt::FunctionPass;
 use crate::*;
@@ -25,9 +27,74 @@ pub struct SimplifyCfgMerge {}
 #[derive(Default)]
 pub struct SimplifyCfgEmpty {}
 
+impl SimplifyCfgConstProp {
+    fn simplify_block_exit(&self, exit: &BlockExit) -> Option<BlockExit> {
+        match exit {
+            BlockExit::ConditionalJump {
+                condition,
+                arg_then,
+                arg_else,
+            } => {
+                if arg_then == arg_else {
+                    return Some(BlockExit::Jump {
+                        arg: arg_then.deref().clone(),
+                    });
+                }
+                if let Some(c) = condition.get_constant() {
+                    match c {
+                        Constant::Int { value: 0, .. } => {
+                            return Some(BlockExit::Jump {
+                                arg: arg_else.deref().clone(),
+                            });
+                        }
+                        Constant::Int { value: 1, .. } => {
+                            return Some(BlockExit::Jump {
+                                arg: arg_then.deref().clone(),
+                            });
+                        }
+                        _ => {}
+                    }
+                }
+                None
+            }
+            BlockExit::Switch {
+                value,
+                default,
+                cases,
+            } => {
+                if cases.iter().all(|(_, jump_to)| default.deref() == jump_to) {
+                    return Some(BlockExit::Jump {
+                        arg: default.deref().clone(),
+                    });
+                }
+                if let Some(v) = value.get_constant() {
+                    return Some(BlockExit::Jump {
+                        arg: if let Some((_, arg)) = cases.iter().find(|(c, _)| v == c) {
+                            arg.clone()
+                        } else {
+                            default.deref().clone()
+                        },
+                    });
+                }
+                None
+            }
+            _ => None,
+        }
+    }
+}
 impl Optimize<FunctionDefinition> for SimplifyCfgConstProp {
-    fn optimize(&mut self, _code: &mut FunctionDefinition) -> bool {
-        todo!("homework 3")
+    fn optimize(&mut self, code: &mut FunctionDefinition) -> bool {
+        code.blocks
+            .iter_mut()
+            .map(|(_, block)| {
+                if let Some(exit) = self.simplify_block_exit(&block.exit) {
+                    block.exit = exit;
+                    true
+                } else {
+                    false
+                }
+            })
+            .any(|r| r)
     }
 }
 
